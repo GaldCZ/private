e540673607f1d3b7cfe7e62345179a4dea6b4c4b
GaldCZ <41269537+GaldCZ@users.noreply.github.com>
2020-03-24 11:42:33 +0100

    Create country_ipsec.php
    PHP v7, allowed just Czech client IP's

---
+++ php/country_ipsec.php
@@ -0,0 +1,31 @@
+<?php
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+$c_ip = get_client_ip();
+$json = file_get_contents('http://ip-api.com/json/'.$c_ip.'?fields=country');
+$obj = json_decode($json, true);
+$country=$obj['country'];
+if ($country != 'Czechia') {
+  echo '404';
+}
+else{
+  echo '200';
+}
